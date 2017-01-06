21102e6a0f42f17bd63d84241145cba6e086917e
user <likhimsiow@yahoo.com>
2017-01-06 16:47:05 +0800

    amended submit button id to enable it by chnage the name

--- app/controllers/brainstree_controller.rb
+++ app/controllers/brainstree_controller.rb
@@ -2,4 +2,43 @@ class BrainstreeController < ApplicationController
   def new
   	@client_token = Braintree::ClientToken.generate
   end
+
+  def checkout
+   	nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
+
+  	result = Braintree::Transaction.sale(
+  		:amount => "10.00", #harcoded. must change later
+  		:payment_method_nonce => nonce_from_the_client,
+  		:options => {
+  			:submit_for_settlement => true
+  		}
+  	)
+
+  	if result.success?
+
+  		redirect_to :root, :flash => { :success => "Transaction successful!" }
+  	else
+  		redirect_to :root, :flash => { :error => "Transaction failed. Please try again." }
+  	end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
